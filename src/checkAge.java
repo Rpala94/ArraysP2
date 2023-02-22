e5c250ff012c3bbd885c6b73adf2c66f61f0f298
Rpala94 <122196216+Rpala94@users.noreply.github.com>
2023-02-22 14:18:40 +0000

    Initial commit

---
+++ src/checkAge.java
@@ -0,0 +1,22 @@
+public class age1 {
+
+    // Create a checkAge() method with an integer variable called age
+    static void	myMethod (int age1) {
+
+        // If age is less than 18, print "Access denied"
+        if(age1<18) {
+            System.out.println("Access denied");
+
+            // If age is greater than, or equal to, 18, print "Access granted"
+        } 		{
+            System.out.println("Access granted");
+        }
+
+    }
+
+    public static void main(String[] args) {
+        // Call the checkAge method and pass along an age of 20
+        (	);
+
+    }
+
